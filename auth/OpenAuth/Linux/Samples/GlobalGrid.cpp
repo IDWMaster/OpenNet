94e70f2d41548d28a87fdc64b46f685403139065
Brian Bosak <webadm@elcnet.servehttp.com>
2015-04-13 20:18:59 -0500

    Added wait period to initialize network

--- auth/OpenAuth/Linux/Samples/GlobalGrid.cpp
+++ auth/OpenAuth/Linux/Samples/GlobalGrid.cpp
@@ -38,7 +38,7 @@ GlobalGrid::InternetProtocol ip(5809,mngr);
 mngr->RegisterProtocol(&ip);
 GGDNS_Init(mngr->nativePtr);
 GGDNS_SetTimeoutInterval(2000);
-
+sleep(1);
 if(argc == 1) {
 	printf("HELP:\ndistauth enumPrivateKeys -- Enumerates private keys\ndistauth makeInternet privateKey -- Makes an Internet and digitally signs it with the specified private key\nsignRecord -- Digitally signs and imports a record piped from STDIN with the specified private key, and exports the signature to stdout.\nenumHosts -- Enumerates hosts for a given authoritative domain by referencing the nearest pointer. Returns a list of GUIDs\ngetDomainPtr -- Retrieves a domain pointer for a specified domain.\nrequestDomain -- requests a domain. The domain must be in the format domainName.parent, where domainName is the child domain being requested, and parent is the pointer to the authoritative object to which the request is being made. Also requires a signing certificate to be specified.\nupdateHost -- Associates host information with the specified domain (requires a pointer to the domain as an argument)\ngetHost -- Retrieves a host record for a specified domain pointer.\nresolveDomain -- Resolves a domain using a specified key.\n");
 
